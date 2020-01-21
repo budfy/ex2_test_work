9ceb39ad609514a9140ebe090b00f7eb471510f4
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2020-01-22 00:01:44 +0200

    Ex2 adaptive ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,38 @@
 $(function () {
+
+  //------------------------------- language switch -------------------------------
   $(".switch__link").click(function () {
-   $('.header__lang-switch').find(".switch__link--current").removeClass("switch__link--current");
-   $(this).addClass("switch__link--current");
+    $('.header__lang-switch').find(".switch__link--current").removeClass("switch__link--current");
+    $(this).addClass("switch__link--current");
   });
+
+  $(".js--burger").click(function () {
+    $('.js--burger').toggleClass("header__burger-menu--active");
+    $('.js--navbar').toggleClass("navbar--active");
+  });
+
+  //------------------------------- scroll to section -------------------------------
+  $("a[href^='#']").click(function () {
+    let target = $(this).attr('href');
+    $("html, body").animate({
+      scrollTop: $(target).offset().top - 50
+    }, {
+      easing: "swing",
+      duration: 1000
+    });
+  });
+
+  $("input[type='radio']").click(
+    function () {
+      let flag = $(this).attr("for");
+      let inputValue = $("#" + flag).val();
+      if (flag == "user__val-1" || "user__val-2") {
+        $('#' + flag).removeAttr('disabled');
+      }
+      if (inputValue == '') {
+        $('#' + flag).removeAttr('disabled');
+      }
+    });
+
+
 });
