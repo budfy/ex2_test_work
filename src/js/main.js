4518d0c92d2c6c45dcb81e8e86f5aacec720c68c
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2020-01-22 02:58:47 +0200

    Ex2 ready (validator working not full)

--- src/js/main.js
+++ src/js/main.js
@@ -22,17 +22,53 @@ $(function () {
     });
   });
 
-  $("input[type='radio']").click(
+  $('.percentage__custom').click(function () {
+    $('.percentage__custom #percentage-4').prop('checked', true);
+    $('.percentage__custom #user__val-1').removeAttr('disabled').focus();
+  })
+  $('.percentage__custom #user__val-1').on('focusin', function () {
+    return 0;
+  });
+  $('.percentage__custom #user__val-1').on('focusout', function () {
+    $(this).prop('disabled', true);
+  });
+
+  $('.level__custom').click(function () {
+    $('.level__custom #level-4').prop('checked', true);
+    $('.level__custom #user__val-2').removeAttr('disabled').focus();
+  })
+  $('.level__custom #user__val-2').on('focusin', function () {
+    return 0;
+  });
+  $('.level__custom #user__val-2').on('focusout', function () {
+    $(this).prop('disabled', true);
+  });
+
+  $('button[type="submit"]').click(
     function () {
-      let flag = $(this).attr("for");
-      let inputValue = $("#" + flag).val();
-      if (flag == "user__val-1" || "user__val-2") {
-        $('#' + flag).removeAttr('disabled');
+      let percentage = $('input[name="percentage"]:checked').val(),
+          percentageUser = $('#user__vaL-1').val(),
+          level = $('input[name="level"]:checked').val(),
+          levelUser = $('#user__vaL-2').val(),
+          email = $('#email').val(),
+    
+      if (percentage.length <1) {
+        alert("select percentage");
       }
-      if (inputValue == '') {
-        $('#' + flag).removeAttr('disabled');
+
+      if (level.length <1) {
+        alert("select level");
       }
-    });
 
-
+      if (percentage == 4 && percentageUser.length <1) {
+        alert("input your percentage or select other");
+      }
+      if (level == 4 && levelUser.length <1) {
+        alert("input your level or select other");
+      }
+      if (email.length <1) {
+        alert("input email");
+      }
+    }
+  )
 });
